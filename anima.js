66ad58ce2bdcd10c5f7d5b70c23d3ba6a26ffdec
will <willamys-g-2001@hotmail.com>
2022-09-11 17:54:16 -0300

    subindo pagina web

---
+++ anima.js
@@ -0,0 +1,35 @@
+function boxHTML() {
+    document.getElementById('box-html').style.animationName = "html"
+    document.getElementById('box-html').style.animationDuration = "0.4s"
+    document.getElementById('box-html').style.animationFillMode = "forwards"
+}
+function noBoxHTML() {
+    document.getElementById('box-html').style.animationName = "html-inverse"
+}
+
+function boxCSS() {
+    document.getElementById('box-css').style.animationName = "css"
+    document.getElementById('box-css').style.animationDuration = "0.4s"
+    document.getElementById('box-css').style.animationFillMode = "forwards"
+}
+function noBoxCSS() {
+    document.getElementById('box-css').style.animationName = "css-inverse"
+}
+
+function boxJS() {
+    document.getElementById('box-js').style.animationName = "js"
+    document.getElementById('box-js').style.animationDuration = "0.4s"
+    document.getElementById('box-js').style.animationFillMode = "forwards"
+}
+function noBoxJS() {
+    document.getElementById('box-js').style.animationName = "js-inverse"
+}
+
+function boxBoot() {
+    document.getElementById('box-boot').style.animationName = "boot"
+    document.getElementById('box-boot').style.animationDuration = "0.4s"
+    document.getElementById('box-boot').style.animationFillMode = "forwards"
+}
+function noBoxBoot() {
+    document.getElementById('box-boot').style.animationName = "boot-inverse"
+}
